2175ef38ae2a579ae58b8d6b9c45d96d931286ba
湯浅なつき <yuasanatsuki@yuasanatsuki-no-MacBook-Air.local>
2019-09-02 21:33:04 +0900

    Finish

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '2964c9f73fc38a76414dcb0eaeac4fc1ccc4862db09f197d53db87786b66e502f116cd375070d725ecc6ba2ce7dbcf33a4c70c71d82eb44ab5024c7617f64959'
